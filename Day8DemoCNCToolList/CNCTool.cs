d91f7b7d211a7867ea346bbb2f52ac231d5f1a81
Rob Garner <rgarner011235@gmail.com>
2021-02-04 18:59:05 -0700

    Add project files.

---
+++ Day8DemoCNCToolList/CNCTool.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day8DemoCNCToolList
+{
+    public class CNCTool
+    {
+        public CNCTool():this(-1, "New Tool created on " + DateTime.Now.ToString("s"),
+            "Flat endmill",3.0f,.25f,1.5f)
+        {
+        }
+
+        /// <summary>
+        /// Create a CNC tool setting some common parameters.
+        /// </summary>
+        /// <param name="id">Unique id for the tool</param>
+        /// <param name="name">Name for the tool</param>
+        /// <param name="type">Type example "Flat" or "Round"</param>
+        /// <param name="length">Total length of the tool</param>
+        /// <param name="diameter">Diameter of cutting part of tool</param>
+        /// <param name="lengthCuttingEdge">Length of just the cutting edge</param>
+        public CNCTool(int id, string name, string type, float length,
+            float diameter, float lengthCuttingEdge):this(id,name,type,length,diameter,
+                lengthCuttingEdge, 2000.0f,3.0f,4)
+        {
+        }
+
+        public CNCTool(int id, string name, string type, float length,
+            float diameter, float lengthCuttingEdge, float rpm, 
+            float feedRate, int numberOfFlutes)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+            Length = length;
+            Diameter = diameter;
+            LengthCuttingEdge = lengthCuttingEdge;
+            this.rpm = rpm;
+            this.feedRate = feedRate;
+            this.numberOfFlutes = numberOfFlutes;
+            CalcChipLoad();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public float Length { get; set; }
+        public float Diameter { get; set; }
+        public float LengthCuttingEdge { get; set; }
+
+        private float rpm;
+        public float RPM
+        {
+            get { return rpm; }
+            set { rpm = value; CalcChipLoad(); }
+        }
+
+        private float feedRate;
+        public float FeedRate
+        {
+            get { return feedRate; }
+            set { feedRate = value; CalcChipLoad(); }
+        }
+
+        private int numberOfFlutes;
+        public int NumberOfFlutes
+        {
+            get { return numberOfFlutes; }
+            set { numberOfFlutes = value; CalcChipLoad(); }
+        }
+
+        public float ChipLoad { get; private set; }
+
+        //public float ChipLoad
+        //{
+        //    get { return FeedRate / (rpm * numberOfFlutes); }
+        //}
+
+
+        private void CalcChipLoad()
+        {
+            ChipLoad = FeedRate/(rpm* numberOfFlutes);
+        }
+
+        public override string ToString()
+        {
+            string str = Name;
+            str += " " + Type;
+            str += " " + Diameter +" in diameter";
+            str += " " + numberOfFlutes + " flutes";
+            return str;
+        }
+
+    }
+}
---
+++ Day8DemoCNCToolList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day8DemoCNCToolList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tool0 = new CNCTool();
+            var tool1 = new CNCTool(1,"Endmill #1","Flat",4.0f,.5f,1.5f);
+            var tool2 = new CNCTool(2, "Endmill #2", "Round", 4.5f, .33f, 2f, 1000, 10, 2);
+            Console.WriteLine(tool0.ToString() + " created!");
+            Console.WriteLine(tool1.ToString() + " created!");
+            Console.WriteLine(tool2.ToString() + " created!");
+
+        }
+    }
+}
